ab3856c9d087151465114584433204669e8e4334
Fabio Angelici <fabio.angelici@outlook.it>
2025-12-21 23:17:20 +0100

    feat(inventory): add ReserveOrderInventory action

---
+++ app/Domains/Inventory/Actions/ReserveOrderInventory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domains\Inventory\Actions;
+
+use App\Domains\Order\Models\Order;
+use Illuminate\Support\Facades\DB;
+
+class ReserveOrderInventory
+{
+    public function __construct(
+        protected ReserveInventory $reserveInventory
+    ) {}     
+    
+    public function execute(Order $order): void
+    {
+        DB::transaction(function () use ($order) {
+            foreach ($order->items as $item) {
+                $this->reserveInventory->execute(
+                    $item->productVariant,
+                    $item->quantity
+                );
+            }
+        });
+    }
+}
